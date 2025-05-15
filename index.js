498ecf9ebe719bc4665b629229a83742a37235c1
LynXHashib <hashib4305@gmail.com>
2025-05-15 14:17:52 +0600

    Add @vercel/analytics dependency and update index.js for integration

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const fs = require("fs");
-const express = require("express");
-const path = require("path");
-const contacts = require("./routes/contactRoute");
+const fs = require('fs');
+const Analytics = require('@vercel/analytics');
+const express = require('express');
+const path = require('path');
+const contacts = require('./routes/contactRoute');
 const app = express();
 app.use(express.static(`${__dirname}/public`));
-const home = require("./routes/homeRoute");
+const home = require('./routes/homeRoute');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", home);
-app.use("/api/contacts", contacts);
+app.use('/', home);
+app.use('/api/contacts', contacts);
 module.exports = app;
