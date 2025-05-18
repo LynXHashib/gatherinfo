ff0de2772a9eae82817276577e7dab4f6238eb6d
LynXHashib <hashib4305@gmail.com>
2025-05-18 18:20:53 +0600

    Wrap server startup logic in a conditional to ensure it only runs when the module is executed directly

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const app = require('./index');
-const dotenv = require('dotenv').config();
-const PORT = process.env.PORT || 2001;
 
-app.listen(PORT, () => {
-  console.log(`Server is Running at http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  const dotenv = require('dotenv').config();
+  const PORT = process.env.PORT || 2001;
+  app.listen(PORT, () => {
+    console.log(`Server is Running at http://localhost:${PORT}`);
+  });
+}
+
 module.exports = app;
